9a860e958173250a8da19ac37629184a1602b935
Sergey Ivanov <boxoficecream@mail.ru>
2021-10-12 15:06:15 +0300

    Homework #1
    - шаблон
    -спрайты
    - первые платформы

---
+++ Assets/Scripts/View/LevelObjectView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+
+	public class LevelObjectView : MonoBehaviour
+	{
+		public Transform _transform;
+		public SpriteRenderer _spriteRenderer;
+		public Collider2D _collider;
+		public Rigidbody2D _rigidbody;
+
+    	void Start()
+    	{
+    	 	 _transform = GetComponent<Transform>();
+    	 	 _spriteRenderer = GetComponent<SpriteRenderer>();
+    	 	// _collider = GetComponent<Collider2D>();
+    	 	// _rigidbody = GetComponent<Rigidbody2d>();
+
+    	}
+
+	}	
+
+}
